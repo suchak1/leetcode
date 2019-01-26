54b7cc2384fb6aa0baca6de821521f21bf35d8e8
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2019-01-25 19:46:58 -0600

    Create reverse_integer.py

---
+++ reverse_integer.py
@@ -0,0 +1,26 @@
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 0:
+          sign = -1
+          x *= -1
+        else:
+          sign = 1;
+
+        y = 0
+        
+        while x != 0:
+            i = x % 10
+            y *= 10
+            y += i
+            x //= 10
+        
+        y *= sign
+        
+        if(y > 2**31-1 or y < -2**31):
+            return 0
+        else:
+            return y
