981aa7a533612780f62d6f6ec6c195d04d7c67f8
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2019-04-01 21:44:25 +0300

    Create jewels_and_stones.py

---
+++ jewels_and_stones.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        d = {}
+        tot = 0
+        
+        for i in S:
+            d[i] = d.get(i, 0) + 1
+
+        
+        for i in J:
+            tot += d.get(i, 0)
+        
+        return tot
+        
+        
