288d1d5ad3cb1c3248becfc1158a315890f11fa5
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2019-04-09 23:44:26 +0300

    Create remove_outermost_parenthesis.py

---
+++ e/remove_outermost_parenthesis.py
@@ -0,0 +1,33 @@
+class Solution:
+    def removeOuterParentheses(self, S: str) -> str:
+        if S == "":
+            return S
+        
+        lcount = 0
+        rcount = 0
+        final = ""
+        start = 0
+        
+        for i, x in enumerate(S):
+            if x == "(":
+                lcount += 1
+            else:
+                rcount += 1
+            
+            if lcount == rcount:
+                final += S[start+1:i]
+                start = i+1
+                lcount = 0
+                rcount = 0
+                
+        return final
+            
+        
+        
+        
+        
+            
+        
+        
+                
+                
