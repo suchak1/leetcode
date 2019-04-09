53188017c7e1376a9654f2b8d3f790649946c6d7
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2019-04-09 22:47:55 +0300

    Create battleship.py

---
+++ m/battleship.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countBattleships(self, board: List[List[str]]) -> int:
+    
+        lastRow = {}
+        last = 0
+        ships = 0
+
+        for i, x in enumerate(board):
+            thisRow = {}
+            
+            for j, y in enumerate(board[i]):
+                if board[i][j] == "X":
+                    thisRow[j] = 1
+                    if j in lastRow or last == 1:
+                        continue
+                    else:
+                        ships += 1
+                        last = 1
+                else:
+                    last = 0
+
+
+            lastRow = thisRow
+            last = 0
+
+
+        return ships
+        
