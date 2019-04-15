1449acacae3c01761bbe16a73d1dce605e744c3d
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2019-04-15 13:13:06 +0300

    Create toeplitz.py

---
+++ e/toeplitz.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        numRows = len(matrix)
+        numCols = len(matrix[0]) if numRows else 0
+        numDiags = numRows + numCols - 1
+        
+        def diag(start, matrix):
+            x = start[0]
+            y = start[1]
+            val = matrix[x][y]
+            
+            while x < len(matrix)-1 and y < len(matrix[0])-1:
+                x += 1
+                y += 1
+                
+                if val != matrix[x][y]:
+                    return False
+            return True
+        
+        for i, x in enumerate(matrix):
+            if not diag([i, 0], matrix):
+                return False
+        
+        for j, y in enumerate(matrix[0]):
+            if j == 0:
+                continue
+            elif not diag([0, j], matrix):
+                return False
+        
+        return True
+                
+        #48s, 79.94%
