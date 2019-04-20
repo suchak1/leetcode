030c450152c6fadedff30dee2a3c2bf9539b60c9
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2019-04-20 17:56:46 +0300

    Update two_sum.py

--- e/two_sum.py
+++ e/two_sum.py
@@ -5,21 +5,12 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
+        
         pos = {}
         
         for i, x in enumerate(nums):
-            if x in pos:
-                pos[x].append(i)
+            if target-x in pos:
+                return [i, pos[target-x][0]]
             else:
                 pos[x] = [i]
-        
-        for x in pos.keys():
-            if target-x in pos:
-                if target-x == x:
-                    if len(pos[x]) == 2:
-                        return [pos[x][0], pos[x][1]]
-                    else:
-                        continue
-                else:
-                    return [pos[x][0], pos[target-x][0]]
-                #40ms, 86.10%
+        #40ms, 86.10%
