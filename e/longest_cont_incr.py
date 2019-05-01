57111c753028ce2c16d99ddf45fa6b209cfaa0ec
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2019-05-02 01:04:28 +0300

    Update longest_cont_incr.py

--- e/longest_cont_incr.py
+++ e/longest_cont_incr.py
@@ -1,12 +1,9 @@
 class Solution:
     def findLengthOfLCIS(self, nums: List[int]) -> int:
         
-        if len(nums) == 0:
-            return 0
-        
         start = 0
-        end = 1
-        longest = 1
+        end = 1 if len(nums) else 0
+        longest = end - start
         
         for i, n in enumerate(nums):
             if nums[i-1] < n:
@@ -15,7 +12,7 @@ class Solution:
                 start = i
             
             if end-start > longest:
-                    longest = end-start
+                    longest = end - start
         return longest
     
         # 44ms, 89.25%
